d6d2d4c246e84fe6277f5a74370858652ebafcd9
yash30201 <54198301+yash30201@users.noreply.github.com>
2022-09-07 18:09:44 +0000

    applied changes for container

--- Private/env.php
+++ Private/env.php
@@ -2,13 +2,13 @@
 
 
 // For running locally
-define("DB_SERVER", "localhost");
-define("DB_USER", "paste_user");
-define("DB_PASS", "paste_password");
-define("DB_NAME", "pasteboard");
-
-// For running in container
-// define("DB_SERVER", "database");
+// define("DB_SERVER", "localhost");
 // define("DB_USER", "paste_user");
 // define("DB_PASS", "paste_password");
 // define("DB_NAME", "pasteboard");
+
+// For running in container
+define("DB_SERVER", "database");
+define("DB_USER", "paste_user");
+define("DB_PASS", "paste_password");
+define("DB_NAME", "pasteboard");
