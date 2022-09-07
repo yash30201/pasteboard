2bc29f07ed8e05d20389f97738be81a48d69da44
yash30201 <54198301+yash30201@users.noreply.github.com>
2022-09-07 17:52:08 +0000

    Edited .gitignore

---
+++ Private/env.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// For running locally
+define("DB_SERVER", "localhost");
+define("DB_USER", "paste_user");
+define("DB_PASS", "paste_password");
+define("DB_NAME", "pasteboard");
+
+// For running in container
+// define("DB_SERVER", "database");
+// define("DB_USER", "paste_user");
+// define("DB_PASS", "paste_password");
+// define("DB_NAME", "pasteboard");
