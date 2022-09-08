96ed9905144572c3367f21e577715e295e6ab3de
yash30201 <54198301+yash30201@users.noreply.github.com>
2022-09-08 11:06:05 +0000

    Changed to run in local container

--- Private/database.php
+++ Private/database.php
@@ -4,31 +4,33 @@ include_once('env.php');
 
 function db_connect()
 {
-    // $instanceUnixSocket = '/cloudsql/yashsahu:asia-south1:pasteboard'; // e.g. '/cloudsql/project:region:instance'
-
 
     // If running in cloud run
-    $instanceUnixSocket = getenv('INSTANCE_UNIX_SOCKET');
-    $db_name = getenv('CLOUD_SQL_DB_NAME');
-    $db_user = getenv('CLOUD_SQL_DB_USER');
-    $db_pass = getenv('CLOUD_SQL_DB_PASS');
+    // $instanceUnixSocket = getenv('INSTANCE_UNIX_SOCKET');
+    // $db_name = getenv('CLOUD_SQL_DB_NAME');
+    // $db_user = getenv('CLOUD_SQL_DB_USER');
+    // $db_pass = getenv('CLOUD_SQL_DB_PASS');
 
     // If running in local
-    // $db_name = DB_NAME;
-    // $db_user = DB_USER;
-    // $db_pass = DB_PASS;
-    // $db_server = DB_SERVER;
+    $db_name = DB_NAME;
+    $db_user = DB_USER;
+    $db_pass = DB_PASS;
+    $db_server = DB_SERVER;
 
     // Connect using UNIX sockets
-    $dsn = sprintf(
-        'mysql:dbname=%s;unix_socket=%s',
-        $db_name,
-        $instanceUnixSocket
-    );
+    // $dsn = sprintf(
+    //     'mysql:dbname=%s;unix_socket=%s',
+    //     $db_name,
+    //     $instanceUnixSocket
+    // );
 
     try {
-        // $connection = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-        $connection = new PDO($dsn, $db_user, $db_pass);
+
+        // For local container run
+        $connection = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+
+        // For cloud run container run
+        // $connection = new PDO($dsn, $db_user, $db_pass);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
     } catch (TypeError $e) {
