53c734bd375af849059973e744b6a482ba3f5cfa
yash30201 <54198301+yash30201@users.noreply.github.com>
2022-09-08 09:48:30 +0000

    Edited database.php for getting env variables

--- Private/database.php
+++ Private/database.php
@@ -3,18 +3,31 @@ require_once('env.php');
 
 function db_connect()
 {
-    $instanceUnixSocket = '/cloudsql/yashsahu:asia-south1:pasteboard'; // e.g. '/cloudsql/project:region:instance'
+    // $instanceUnixSocket = '/cloudsql/yashsahu:asia-south1:pasteboard'; // e.g. '/cloudsql/project:region:instance'
+
+
+    // If running in cloud run
+    $instanceUnixSocket = getenv('INSTANCE_UNIX_SOCKET');
+    $db_name = getenv('CLOUD_SQL_DB_NAME');
+    $db_user = getenv('CLOUD_SQL_DB_USER');
+    $db_pass = getenv('CLOUD_SQL_DB_PASS');
+
+    // If running in local
+    // $db_name = DB_NAME;
+    // $db_user = DB_USER;
+    // $db_pass = DB_PASS;
+    // $db_server = DB_SERVER;
 
     // Connect using UNIX sockets
     $dsn = sprintf(
         'mysql:dbname=%s;unix_socket=%s',
-        DB_NAME,
+        $db_name,
         $instanceUnixSocket
     );
 
     try {
         // $connection = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-        $connection = new PDO($dsn, DB_USER, DB_PASS);
+        $connection = new PDO($dsn, $db_user, $db_pass);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
     } catch (TypeError $e) {
